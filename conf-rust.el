7a0e1c70f01ecb767a32776e7503fc3e10349a0b
Cyprien Le Pannérer <cyplp@free.fr>
2018-08-02 19:24:51 +0200

    more stuff for rust

--- conf-rust.el
+++ conf-rust.el
@@ -6,3 +6,14 @@
 
 (with-eval-after-load 'rust-mode
   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
+;; completion and doc
+(use-package racer
+  :ensure t)
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+
+;; cargo
+(use-package cargo
+  :ensure t)
+(add-hook 'rust-mode-hook 'cargo-minor-mode)
