02f863cf1998c3edc93ffdec29b6fd164245e36e
Cyprien Le Pannérer <clepannerer@edd.fr>
2019-03-27 09:43:41 +0100

    fix file+datetree on file+olp+datetree

--- conf-org.el
+++ conf-org.el
@@ -9,25 +9,25 @@
 (setq org-capture-templates
       '(("a" "Activité"
 	 entry
-	 (file+datetree "~/dev/log_cyp/activities.org")
+	 (file+olp+datetree "~/dev/log_cyp/activities.org")
 	 "* %?"
 	 :clock-in t
 	 :clock-resume t)
-      ("t" "todo" entry (file+datetree "~/dev/log_cyp/activities.org")
+      ("t" "todo" entry (file+olp+datetree "~/dev/log_cyp/activities.org")
        "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
-      ("r" "respond" entry (file+datetree "~/dev/log_cyp/activities.org")
+      ("r" "respond" entry (file+olp+datetree "~/dev/log_cyp/activities.org")
        "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
-      ("n" "note" entry (file+datetree "~/dev/log_cyp/activities.org")
+      ("n" "note" entry (file+olp+datetree "~/dev/log_cyp/activities.org")
        "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
       ("j" "Journal" entry (file+datetree+datetree "~/dev/log_cyp/diary.org")
        "* %?\n%U\n" :clock-in t :clock-resume t)
-      ("w" "org-protocol" entry (file+datetree "~/dev/log_cyp/activities.org")
+      ("w" "org-protocol" entry (file+olp+datetree "~/dev/log_cyp/activities.org")
        "* TODO Review %c\n%U\n" :immediate-finish t)
-      ("m" "Meeting" entry (file+datetree "~/dev/log_cyp/activities.org")
+      ("m" "Meeting" entry (file+olp+datetree "~/dev/log_cyp/activities.org")
        "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
-      ("p" "Phone call" entry (file+datetree "~/dev/log_cyp/activities.org")
+      ("p" "Phone call" entry (file+olp+datetree "~/dev/log_cyp/activities.org")
        "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
-      ("h" "Habit" entry (file+datetree "~/dev/log_cyp/activities.org")
+      ("h" "Habit" entry (file+olp+datetree "~/dev/log_cyp/activities.org")
        "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n")))
 
 (global-set-key [f6]
