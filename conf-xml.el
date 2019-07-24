1e669d26af53cafa74f1778abf845453e526d3fa
Cyprien Le Pannérer <clepannerer@edd.fr>
2019-07-24 10:35:38 +0200

    add fold xml

--- conf-xml.el
+++ conf-xml.el
@@ -16,3 +16,26 @@
 ;;could be usefull sometimes
 (use-package x-path-walker
   :ensure t)
+
+(use-package hideshow
+  :ensure t)
+
+(use-package sgml-mode
+  :ensure t)
+
+
+;; fold some part
+;; https://emacs.stackexchange.com/questions/2884/the-old-how-to-fold-xml-question
+(add-to-list 'hs-special-modes-alist
+             '(nxml-mode
+               "<!--\\|<[^/>]*[^/]>"
+               "-->\\|</[^/>]*[^/]>"
+
+               "<!--"
+               sgml-skip-tag-forward
+               nil))
+
+(add-hook 'nxml-mode-hook 'hs-minor-mode)
+
+;; optional key bindings, easier than hs defaults
+(define-key nxml-mode-map (kbd "C-c h") 'hs-toggle-hiding)
