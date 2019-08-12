4f907e21a4b58d577b1852243ca70685449b2f46
Cyprien Le Pannérer <cyplp@free.fr>
2019-08-12 13:51:02 +0200

    remap uncomment

--- shortcut.el
+++ shortcut.el
@@ -9,7 +9,7 @@
 (global-set-key [f3] 'revert-buffer)
 (global-set-key [f4] 'kill-this-buffer)
 (global-set-key [f5] 'comment-region)
-(global-set-key [f7] 'uncomment-region)
+(global-set-key "\S-F5" 'uncomment-region)
 (global-set-key [f12] 'flycheck-display-error-at-point)
 (global-set-key [delete] 'delete-char)
 ;;(global-set-key [home] 'beginning-of-buffer)
