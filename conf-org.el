055af05c28bf4c55f3c51893633ed321dfe6bf01
Cyprien Le Pannérer <cyprien.le.pannerer@adaptive-channel.com>
2019-10-17 13:52:35 +0200

    fix template

--- conf-org.el
+++ conf-org.el
@@ -15,9 +15,9 @@
 	 :clock-resume t)
 	("t" "todo" entry
 	 (file "~/dev/log_cyp/todos.org")
-	 "\n** TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+	 "\n* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
 	("T" "todo whitout file" item (file "~/dev/log_cyp/todos.org")
-	 "\n** TODO %?\n%U\n" :clock-in t :clock-resume t)
+	 "\n* TODO %?\n%U\n" :clock-in t :clock-resume t)
 	("p" "Phone call" entry (file+olp+datetree "~/dev/log_cyp/activities.org")
 	 "\n* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
 ))
