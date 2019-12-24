6ee0ffc9294da77d355d1a3e8ce12bc8c7e7e301
Cyprien Le Pannérer <cyprien.le.pannerer@adaptive-channel.com>
2019-12-24 11:46:21 +0100

    cosmit

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+;;; init.el --- -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
 (require 'package)
 (add-to-list 'package-archives
 	     '("marmalade" . "http://marmalade-repo.org/packages/"))
