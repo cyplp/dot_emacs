8a55ee6e320b90dbbfc732c51a22bc04295835fb
Cyprien Le Pannérer <cyprien.le.pannerer@adaptive-channel.com>
2020-04-14 08:29:26 +0200

    add roam-mode

--- conf-org.el
+++ conf-org.el
@@ -148,3 +148,15 @@
                  (org-remove-inline-images)
                  (org-present-show-cursor)
                  (org-present-read-write)))))
+
+(use-package org-roam
+      :ensure t
+      :hook (after-init . org-roam-mode)
+      :custom (org-roam-directory "~/dev/log_cyp/")
+      :bind (:map org-roam-mode-map
+              (("C-c n l" . org-roam)
+               ("C-c n f" . org-roam-find-file)
+               ("C-c n b" . org-roam-switch-to-buffer)
+               ("C-c n g" . org-roam-show-graph))
+              :map org-mode-map
+              (("C-c n i" . org-roam-insert))))
