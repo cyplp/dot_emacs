fefdecb354f203b517d12ee47460aa274f608dc1
Cyprien Le Pannérer <cyplp@free.fr>
2020-06-22 15:45:40 +0200

    fix old command for new one

--- conf-org.el
+++ conf-org.el
@@ -171,7 +171,7 @@
               (("C-c n l" . org-roam)
                ("C-c n f" . org-roam-find-file)
                ("C-c n b" . org-roam-switch-to-buffer)
-               ("C-c n g" . org-roam-show-graph))
+               ("C-c n g" . org-roam-graph-show))
               :map org-mode-map
               (("C-c n i" . org-roam-insert))))
 
