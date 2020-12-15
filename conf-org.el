a8822cb8c23e5c29dcd495718f93c219bd39a6ef
Cyprien Le Pannérer <cyprien.le.pannerer@adaptive-channel.com>
2020-12-15 13:58:47 +0100

    fix shorcur

--- conf-org.el
+++ conf-org.el
@@ -172,7 +172,7 @@
               (("C-c n l" . org-roam)
                ("C-c n f" . org-roam-find-file)
                ("C-c n b" . org-roam-switch-to-buffer)
-               ("C-c n g" . org-roam-graph-show))
+               ("C-c n g" . org-roam-graph))
               :map org-mode-map
               (("C-c n i" . org-roam-insert))))
 
