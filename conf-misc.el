aa0bc659175d7e5614cd15f9243b04a16104c591
Cyprien Le Pannérer <c.le.pannerer@qotto.net>
2021-03-12 14:41:48 +0100

    imporv insert uuid

--- conf-misc.el
+++ conf-misc.el
@@ -324,27 +324,19 @@
     :init (progn  (add-hook 'prog-mode-hook 'highlight-symbol-mode)
                   (add-hook 'prog-mode-hook 'highlight-symbol-nav-mode)))
 
+;; copy from http://ergoemacs.org/emacs/elisp_read_file_content.html
+(defun get-string-from-file (filePath)
+  "Return filePath's file content."
+  (with-temp-buffer
+    (insert-file-contents filePath)
+    (string-trim (buffer-string))))
+
 ;; uuid generator
 ;; copy from https://nullprogram.com/blog/2010/05/11/
 (defun uuid-create ()
   "Return a newly generated UUID. This uses a simple hashing of variable data."
-  (let ((s (md5 (format "%s%s%s%s%s%s%s%s%s%s"
-                        (user-uid)
-                        (emacs-pid)
-                        (system-name)
-                        (user-full-name)
-                        user-mail-address
-                        (current-time)
-                        (emacs-uptime)
-                        (garbage-collect)
-                        (random)
-                        (recent-keys)))))
-    (format "%s-%s-3%s-%s-%s"
-            (substring s 0 8)
-            (substring s 8 12)
-            (substring s 13 16)
-            (substring s 16 20)
-            (substring s 20 32))))
+  (get-string-from-file "/proc/sys/kernel/random/uuid")
+  )
 
 (defun uuid-insert ()
   "Inserts a new UUID at the point."
