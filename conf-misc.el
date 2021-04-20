32ab9eb940c465ec4ad286692cfaef1140ae76b2
Cyprien Le Pannérer <c.le.pannerer@qotto.net>
2021-04-20 15:01:50 +0200

    add markdown support

--- conf-misc.el
+++ conf-misc.el
@@ -402,5 +402,15 @@
   :ensure t)
 
 (global-whitespace-cleanup-mode t)
+
+;; markdown
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
 (provide 'conf-misc)
 ;;; conf-misc ends here
